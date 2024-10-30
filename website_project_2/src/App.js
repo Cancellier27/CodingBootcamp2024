30e674120828d2e37ce726e6d5a9f3b7435f5700
Cancellier27 <filipecancelliercosta@gmail.com>
2024-10-30 21:22:07 +0000

    created about section and fixed the home section

--- website_project_2/src/App.js
+++ website_project_2/src/App.js
@@ -1,18 +1,65 @@
 import "./App.css"
 import Footer from "./components/footer"
 import Nav from "./components/nav"
+import jsImage from "./images/js-image.jpg"
 
 function App() {
   return (
     <div id="general-container">
       <Nav />
       <div className="main-section">
-        <section id="home" >
+        <section id="home">
           <h1>Hi, I am Filipe!</h1>
-          <h4>A Civil Engineer in career transition to become a web developer</h4>
+          <h4>
+            A Civil Engineer in career transition to become a web developer
+          </h4>
         </section>
-        <section id="about"  >ABOUT</section>
-        <section id="projects"  >PROJECT</section>
+        <section id="about">
+          <h1 className="about-title">ABOUT</h1>
+          <p className="about-text">
+            <img className="about-image" src={jsImage} alt="js coding general images, with if and else"></img>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            <br></br>
+            <br></br>
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. <br></br>
+            <br></br>
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </section>
+        <section id="projects">PROJECT</section>
       </div>
       <Footer />
     </div>
